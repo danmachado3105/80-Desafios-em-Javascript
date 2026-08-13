9f83c4083ce2227c69fc7004f757711606b278df
Danilo Machado <225914095+danmachado3105@users.noreply.github.com>
2026-08-13 20:34:18 -0300

    Add desafios 12 e 13

---
+++ Desafios/Desafio_12.js
@@ -0,0 +1,8 @@
+// Desafio 12 - Situação do aluno
+
+const nota1 = 8;
+const nota2 = 6;
+
+const media = (nota1 + nota2) / 2;
+
+console.log(media >= 7);
---
+++ Desafios/Desafio_13.js
@@ -0,0 +1,8 @@
+// Desafio 13 - Simulação de troco
+
+const valorCompra = 35.90;
+const valorPago = 50;
+
+const troco = valorPago - valorCompra;
+
+console.log(`O troco a ser devolvido é R$ ${troco.toFixed(2)}.`);
